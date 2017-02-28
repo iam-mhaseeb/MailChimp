5a3bf931c3c0fb55aa3c98475a88b3586af7e86b
Muhammad Haseeb <haseeb.emailbox@gmail.com>
2017-02-28 13:51:35 +0500

    MailChimp Added

--- app/src/main/java/com/hsblabs/mailchimp/MailChimp.java
+++ app/src/main/java/com/hsblabs/mailchimp/MailChimp.java
@@ -1,10 +1,19 @@
 package com.hsblabs.mailchimp;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+
+import rsg.mailchimp.api.MailChimpApiException;
+import rsg.mailchimp.api.lists.ListMethods;
+
+import static java.security.AccessController.getContext;
 
 public class MailChimp extends AppCompatActivity {
 
@@ -19,8 +28,43 @@ public class MailChimp extends AppCompatActivity {
         bt.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                Runnable run = new Runnable() {
+                    public void run() {
+                        if (et.getText() != null && et.getText().toString().trim().length() > 0) {
+                            addToList(et.getText().toString());
+                        }
+                    }
+                };
+                (new Thread(run)).start();
             }
         });
+    }
+    private void addToList(String emailAddy) {
+
+        ListMethods listMethods = new ListMethods("28689164390c8ddf980de203ed9842d7-us15");
+        String message = "Signup successful!";
+        try {
+            listMethods.listSubscribe("667bd894f3", emailAddy);
+        } catch (MailChimpApiException e) {
+            Log.e("MailChimp", "Exception subscribing person: " + e.getMessage());
+            message = "Signup failed: " + e.getMessage();
+        } finally {
+            showResult(message);
+        }
+
+    }
+    private void showResult(final String message) {
+        Runnable run = new Runnable() {
+            public void run() {
+                AlertDialog.Builder builder = new AlertDialog.Builder(MailChimp.this);
+                builder.setMessage(message).setPositiveButton("OK", new Dialog.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
+                builder.create().show();
+            }
+        };
+        MailChimp.this.runOnUiThread(run);
     }
 }
