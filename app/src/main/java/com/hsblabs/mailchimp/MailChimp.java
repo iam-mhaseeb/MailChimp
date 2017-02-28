c211abbe82777ae7fb99a9241221b0b6cf3ddd5f
Muhammad Haseeb <haseeb.emailbox@gmail.com>
2017-02-28 12:06:49 +0500

    Initial Commit

---
+++ app/src/main/java/com/hsblabs/mailchimp/MailChimp.java
@@ -0,0 +1,13 @@
+package com.hsblabs.mailchimp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MailChimp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mail_chimp);
+    }
+}
