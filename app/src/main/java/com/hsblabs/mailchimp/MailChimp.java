012f3fab3148b768e8c2611bb30c6ed3f93849e0
Muhammad Haseeb <haseeb.emailbox@gmail.com>
2017-02-28 12:31:56 +0500

    MailChimp Api Added

--- app/src/main/java/com/hsblabs/mailchimp/MailChimp.java
+++ app/src/main/java/com/hsblabs/mailchimp/MailChimp.java
@@ -2,12 +2,15 @@ package com.hsblabs.mailchimp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MailChimp extends AppCompatActivity {
 
+    EditText et;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mail_chimp);
+        et = (EditText) findViewById(R.id.editText);
     }
 }
